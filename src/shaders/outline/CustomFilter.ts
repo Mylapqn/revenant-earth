532f70d677a4e92a85fd629aaa8f57c1bb475b59
Andrej Karovin <andrej.karovin2000@gmail.com>
2023-03-16 19:29:27 +0100

    feat: load frag from separate file

--- src/shaders/outline/CustomFilter.ts
+++ src/shaders/outline/CustomFilter.ts
@@ -1,51 +1,9 @@
-//import fragment from './outline.frag';
+const fs = require("fs");
 import { Filter, utils } from '@pixi/core';
 import type { FilterSystem, RenderTexture, CLEAR_MODES } from '@pixi/core';
 import { Assets, Color } from 'pixi.js';
 
-let fragment: string = `precision mediump float;
-varying vec2 vTextureCoord;
-uniform sampler2D uSampler;
-uniform vec4 filterClamp;
-
-uniform float uAlpha;
-uniform vec2 uThickness;
-uniform vec4 uColor;
-uniform bool uKnockout;
-
-const float DOUBLE_PI = 2. * 3.14159265358979323846264;
-const float MIN_ANGLE = \${minAngle};
-const float MAX_ANGLE = \${maxAngle};
-const float ANGLE_STEP = \${angleStep};
-
-float outlineMaxAlphaAtPos(vec2 pos,vec4 sourceColor) {
-    if (uThickness.x == 0. || uThickness.y == 0.) {
-        return 0.;
-    }
-
-    vec4 displacedColor;
-    vec2 displacedPos;
-    float maxAlpha = 0.;
-
-    for (float angle = MIN_ANGLE; angle <= MAX_ANGLE; angle += ANGLE_STEP) {
-        displacedPos.x = vTextureCoord.x + uThickness.x * cos(angle);
-        displacedPos.y = vTextureCoord.y + uThickness.y * sin(angle);
-        displacedColor = texture2D(uSampler, clamp(displacedPos, filterClamp.xy, filterClamp.zw));
-        maxAlpha = max(maxAlpha, sourceColor.a-displacedColor.a);
-    }
-
-    return maxAlpha;
-}
-
-void main(void) {
-    vec4 sourceColor = texture2D(uSampler, vTextureCoord);
-    vec4 contentColor = sourceColor * float(!uKnockout);
-    float outlineAlpha = uAlpha * outlineMaxAlphaAtPos(vTextureCoord.xy,sourceColor);
-    vec4 outlineColor = vec4(vec3(uColor) * outlineAlpha, 0.);
-    gl_FragColor = contentColor + outlineColor;
-}
-
-`;
+let fragment: string = fs.readFileSync(__dirname + '/outline.frag', 'utf8');
 
 /**
  * OutlineFilter, originally by mishaa
